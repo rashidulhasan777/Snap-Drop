8ee465d06ebc0ded38d6f10214d9e4567f94bfe6
Sajon <sajon5271@gmail.com>
2023-03-18 22:53:51 +0600

    Base structure created

---
+++ server/index.js
@@ -0,0 +1,18 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv');
+}
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+(async function () {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    app.listen(process.env.PORT || 3000, () => {
+      console.log('Server started on ' + (process.env.PORT || 3000));
+    });
+  } catch (error) {
+    console.log(error);
+  }
+})();
