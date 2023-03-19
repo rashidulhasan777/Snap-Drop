32156d3e9e82d7c7407eb639554fa70f85ba2d24
Sajon <sajon@gmail.com>
2023-03-19 19:49:29 +0600

    Fixed conflicts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,13 +7,18 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 //Components
 import { AppComponent } from './app.component';
 import { GoogleLoginComponent } from './components/google-login/google-login.component';
-<<<<<<< HEAD
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialComponentsModule } from './material-components/material-components.module';
 import { LoginComponent } from './components/login/login.component';
+import { FacebookLoginComponent } from './components/facebook-login/facebook-login.component';
 
 @NgModule({
-  declarations: [AppComponent, GoogleLoginComponent, LoginComponent],
+  declarations: [
+    AppComponent,
+    GoogleLoginComponent,
+    LoginComponent,
+    FacebookLoginComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -21,14 +26,6 @@ import { LoginComponent } from './components/login/login.component';
     BrowserAnimationsModule,
     MaterialComponentsModule,
   ],
-=======
-import { FacebookLoginComponent } from './components/facebook-login/facebook-login.component';
-
-@NgModule({
-  declarations: [AppComponent, GoogleLoginComponent, FacebookLoginComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
->>>>>>> facebook-oauth
-  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
