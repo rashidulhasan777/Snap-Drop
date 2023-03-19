290b92a3ae44c7573ed4c4e96ddfc5a11b596222
rafsan0230 <rafsan0230@gmail.com>
2023-03-19 11:42:25 +0600

    dotenv added in index

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const router = require('./routes');
+
 
 const corsConfig = {
   origin: 'http://localhost:4200',
