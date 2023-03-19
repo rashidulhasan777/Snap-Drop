9297db60765b5673df72c3f1370e3b858ff73e98
afahad0149 <fahad.akibahmed@gmail.com>
2023-03-19 16:16:05 +0600

    google oauth implemented

--- server/controllers/oauth.controller.js
+++ server/controllers/oauth.controller.js
@@ -2,34 +2,23 @@ const axios = require('axios');
 
 const googleAccessCode = async (req, res, next) => {
   const { code } = req.body;
+  console.log(code);
   const url = new URL('https://oauth2.googleapis.com/token');
   url.searchParams.append('client_id', process.env.GOOGLE_CLIENT_ID);
   url.searchParams.append('client_secret', process.env.GOOGLE_CLIENT_SECRET);
   url.searchParams.append('code', code);
   url.searchParams.append('grant_type', 'authorization_code');
-  url.searchParams.append(
-    'redirect_uri',
-    'http://localhost:4200/google_oauth'
-  );
+  url.searchParams.append('redirect_uri', 'http://localhost:4200/oauth_google');
   try {
-    const access_token = await axios.post(
-      url.toString(),
-      {},
-      {
-        headers: {
-          Accept: 'application/json',
-        },
-      }
-    );
+    const access_token = await axios.post(url.toString(), {});
     res.status(200);
     console.log(access_token.data);
     res.send({ access_token: access_token.data.access_token });
   } catch (error) {
-    // console.log(error);
+    console.log(error);
     res.status(500);
-    res.send({ error: '500', message: 'Something went wrong' });
+    res.send({ errorMessage: 'Something went wrong' });
   }
 };
-
 
 module.exports = { googleAccessCode };
--- server/db.js
+++ server/db.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 (async function () {
-    try {
-      await mongoose.connect(process.env.DB_URL);
-        console.log(`Database connected @ port ${process.env.PORT || 3000}!`);
-    } catch (error) {
-      console.log(`Something went wrong in Database! ${err}`);
-    }
-  })();
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    console.log(`Database connected`);
+  } catch (error) {
+    console.log(`Something went wrong in Database! ${err}`);
+  }
+})();
 
-  module.exports = mongoose;
+module.exports = mongoose;
