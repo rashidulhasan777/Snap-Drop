f8570eef075188c9f4b2f9a1e4556a411b483ad9
rafsan0230 <rafsan0230@gmail.com>
2023-03-26 10:36:10 +0600

    order status functional

--- client/src/app/components/order-status/order-status.component.ts
+++ client/src/app/components/order-status/order-status.component.ts
@@ -1,13 +1,45 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
+import { OrderService } from 'src/app/services/orders/order.service';
+import { Order } from 'src/app/interfaces/order.interface';
+
+
 
 @Component({
   selector: 'app-order-status',
   templateUrl: './order-status.component.html',
   styleUrls: ['./order-status.component.css']
 })
-export class OrderStatusComponent {
-  constructor(private _formBuilder: FormBuilder) {}
+export class OrderStatusComponent implements OnInit{
+  // user?: User;
+  order!:Order;
+  status:string ='';
+
+
+  constructor(private _formBuilder: FormBuilder, private orderService : OrderService) {}
   firstFormGroup: FormGroup = this._formBuilder.group({firstCtrl: ['']});
   secondFormGroup: FormGroup = this._formBuilder.group({secondCtrl: ['']});
+
+  ngOnInit(){
+    // const user = this.userService.getUser().subscribe((response)=> {
+    //   this.user= response;
+    // }) 
+    const userOrder = this.orderService.getCustomerLatestOrder().subscribe((response) =>{
+      this.order = response;
+      if(this.order.orderStatus == "pending"){
+        this.status='0';
+      };
+      if(this.order.orderStatus == "approved"){
+        this.status='1';
+      };
+      if(this.order.orderStatus == "printing"){
+        this.status='2';
+      };
+      if(this.order.orderStatus == "readyToDeliver"){
+        this.status='3';
+        
+      };
+    })
+  }
+  
 }
