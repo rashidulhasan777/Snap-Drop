aa06d8c24679a950693fa5f042e2c892067bc98b
Sajon <sajon@gmail.com>
2023-03-29 13:05:50 +0600

    Deployy

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,7 +1,7 @@
-// export const baseBackendURL = 'https://snapdropbd.fly.dev';
-export const baseBackendURL = 'http://localhost:3000';
-// export const baseFrontendURL = 'https://snap-drop.vercel.app';
-export const baseFrontendURL = 'http://localhost:4200';
+export const baseBackendURL = 'https://snapdropbd.fly.dev';
+// export const baseBackendURL = 'http://localhost:3000';
+export const baseFrontendURL = 'https://snap-drop.vercel.app';
+// export const baseFrontendURL = 'http://localhost:4200';
 
 export const VAPID_PUBLIC_KEY =
   'BGWbwwGSeOp32TFIezGDGCejWllTBcMpn3ASg6wbBp9Uxx14GPdiA9YKoHyg2gGMrcKeAlRtAnpj15OJXZu2S6w';
