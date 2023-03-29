0e36c8d2f26df2968db3fb4069cf52324f441293
afahad0149 <fahad.akibahmed@gmail.com>
2023-03-29 09:38:07 +0600

    updated stylings for delivery address forms

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,4 +1,4 @@
-export const baseBackendURL = 'https://snapdropbd.fly.dev';
-// export const baseBackendURL = 'http://localhost:3000';
-export const baseFrontendURL = 'https://snap-drop.vercel.app';
-// export const baseFrontendURL = 'http://localhost:4200';
+// export const baseBackendURL = 'https://snapdropbd.fly.dev';
+export const baseBackendURL = 'http://localhost:3000';
+// export const baseFrontendURL = 'https://snap-drop.vercel.app';
+export const baseFrontendURL = 'http://localhost:4200';
