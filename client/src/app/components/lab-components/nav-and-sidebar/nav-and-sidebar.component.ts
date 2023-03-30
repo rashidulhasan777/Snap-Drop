40d7b276021ad4c8a9c634db2753d49c17a39703
rafsan0230 <rafsan0230@gmail.com>
2023-03-30 22:20:21 +0600

    tried active page shadow

--- client/src/app/components/lab-components/nav-and-sidebar/nav-and-sidebar.component.ts
+++ client/src/app/components/lab-components/nav-and-sidebar/nav-and-sidebar.component.ts
@@ -9,6 +9,7 @@ import { OrderService } from 'src/app/services/orders/order.service';
   styleUrls: ['./nav-and-sidebar.component.css'],
 })
 export class NavAndSidebarComponent {
+  selectedButton: string="button1";
   @ViewChild('sidenav') sidenav!: MatSidenav;
   isExpanded = true;
   isShowing = false;
