a1853144f280a61cef91ccf1dcabfb998f227c83
Sajon <sajon@gmail.com>
2023-03-30 21:32:01 +0600

    Fixed assets, maybe

--- client/src/app/components/background/background.component.ts
+++ client/src/app/components/background/background.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-background',
   template: '<div class="background"><ng-content></ng-content></div>',
-  styles: ['.background { background-image: url("../../../assets/moon.svg"); background-size: cover; overflow: scroll; position: fixed; top: 0; left: 0; right: 0; bottom: 0; }']
+  styles: ['.background { background-image: url("/assets/moon.svg"); background-size: cover; overflow: scroll; position: fixed; top: 0; left: 0; right: 0; bottom: 0; }']
 })
 export class BackgroundComponent {
  //filter: blur(5px); z-index: -1; 
