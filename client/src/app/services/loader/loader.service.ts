b3288d39bc2235a1623b9fa5954474f2b9c3fb33
Sajon <sajon@gmail.com>
2023-03-31 23:02:10 +0600

    litle future update

--- client/src/app/services/loader/loader.service.ts
+++ client/src/app/services/loader/loader.service.ts
@@ -7,6 +7,7 @@ export class LoaderService {
   private loading: boolean = false;
   private loadingMsg = '';
 
+  private blockNavbar = false;
   constructor() {}
 
   setLoading(loading: boolean) {
@@ -22,5 +23,12 @@ export class LoaderService {
 
   getLoadingMsg(): string {
     return this.loadingMsg;
+  }
+
+  setBlockNavbar(block: boolean) {
+    this.blockNavbar = block;
+  }
+  getBlockNavbar() {
+    return this.blockNavbar;
   }
 }
