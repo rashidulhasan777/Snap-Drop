9726027069b1bce8f6485eab0423ad1b550ec588
krbappy00 <seo15773@gmail.com>
2023-11-12 18:09:38 +0600

    modified:userLocationFInder in nearestLabFinder and add lab id in order-summary component

--- server/utils/helpers/nearestLabFinder.js
+++ server/utils/helpers/nearestLabFinder.js
@@ -5,18 +5,26 @@ const client = new Client({});
 
 const userLocationFinder = async (country, city, zone, area = '') => {
   console.log(`${area ? area + ',' : ''} ${zone}, ${city}, ${country}`);
-  const args = {
-    params: {
-      key: process.env.MAPS_API_KEY,
-      address: `${area ? area + ',' : ''} ${zone}, ${city}, ${country}`,
-    },
-  };
-  try {
-    const gcResponse = await client.geocode(args);
-    const userLocation = gcResponse.data.results[0].geometry.location;
-    return userLocation;
-  } catch (err) {
-    console.log(err);
+  // const args = {
+  //   params: {
+  //     key: process.env.MAPS_API_KEY,
+  //     address: `${area ? area + ',' : ''} ${zone}, ${city}, ${country}`,
+  //   },
+  // };
+  // try {
+  //   const gcResponse = await client.geocode(args);
+  //   console.log("from distance response",gcResponse)
+  //   const userLocation = gcResponse.data.results[0].geometry.location;
+  //   return userLocation;
+  // } catch (err) {
+  //   console.log({err:"kajdshfkasjdfhls"});
+  // }
+  try{
+    const responseFromMapbox = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/{${zone}}.json?access_token=pk.eyJ1IjoiYXNpZnVycmFobWFucGlhbCIsImEiOiJjbG9oaG0zMnQxZDVhMm1yd3c1bTVwZWQ4In0.HjT23K3i1ur6Jy9KaikNEw`);
+    const data = await responseFromMapbox.json();
+    return {lng: data.features[0].geometry.coordinates[0], lat: data.features[0].geometry.coordinates[1]};
+  } catch(err){
+    console.log("hello error",err)
   }
 };
 
@@ -32,7 +40,7 @@ async function findClosestStudio(area, zone, city, country) {
     const allLabs = await Lab.find({});
     const coordinates = await userLocationFinder(country, city, zone, area);
     const point = { coordinates };
-    // console.log(point);
+    console.log("from userDistance coordinates",point);
     const labsCoords = allLabs.map((studio) => {
       return { coordinates: { lng: studio.long, lat: studio.lat } };
     });
@@ -48,7 +56,7 @@ async function findClosestStudio(area, zone, city, country) {
         closest.distance < distances[idx].distance ? studio : idx,
       0
     );
-
+    console.log("from userDistance result",result);
     return allLabs[result];
   } catch (error) {
     console.log(error);
