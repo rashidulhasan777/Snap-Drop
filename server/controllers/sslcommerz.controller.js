fdf8547aef4c2374c026c952a8bd2df6dde18a49
krbappy00 <seo15773@gmail.com>
2023-11-17 02:10:50 +0600

    REFACTOR:all query are separated from controller

--- server/controllers/sslcommerz.controller.js
+++ server/controllers/sslcommerz.controller.js
@@ -1,71 +1,26 @@
 const SSLCommerzPayment = require("sslcommerz-lts");
+const { paymentInitialize, getSucess } = require("../models/sslCommerz/query");
 const is_live = false;
 
-const initPayment = (req, res) => {
-  console.log("here");
+const initPayment = async(req, res) => {
   const amount = req.params.amount;
   const order_id = req.params.order_id;
-
-  const data = {
-    total_amount: amount,
-    currency: "BDT",
-    tran_id: order_id, // use unique tran_id for each api call
-    success_url: "http://localhost:8080/payment-success",
-    fail_url: "http://localhost:8080/payment-failure",
-    cancel_url: "http://localhost:8080/payment-cancel",
-    ipn_url: "http://localhost:8080/payment-ipn",
-    shipping_method: "Courier",
-    product_name: "Computer.",
-    product_category: "Electronic",
-    product_profile: "general",
-    cus_name: "Customer Name",
-    cus_email: "customer@example.com",
-    cus_add1: "Dhaka",
-    cus_add2: "Dhaka",
-    cus_city: "Dhaka",
-    cus_state: "Dhaka",
-    cus_postcode: "1000",
-    cus_country: "Bangladesh",
-    cus_phone: "01711111111",
-    cus_fax: "01711111111",
-    ship_name: "Customer Name",
-    ship_add1: "Dhaka",
-    ship_add2: "Dhaka",
-    ship_city: "Dhaka",
-    ship_state: "Dhaka",
-    ship_postcode: 1000,
-    ship_country: "Bangladesh",
-  };
-  const sslcz = new SSLCommerzPayment(
-    process.env.STORE_ID,
-    process.env.STORE_PASSWORD,
-    is_live
-  );
-  sslcz.init(data).then((apiResponse) => {
-    // Redirect the user to payment gateway
-    let GatewayPageURL = apiResponse.GatewayPageURL;
-    // res.redirect(GatewayPageURL);
+  try{
+    const GatewayPageURL =await paymentInitialize(amount, order_id)
     res.status(200).send({ url: GatewayPageURL });
-    console.log("Redirecting to: init ", GatewayPageURL);
-  });
+  } catch(error){
+    res.status(500).send({ errorMessage: 'Something went wrong in payment' });
+  }
+    
 };
-//FAILED
-//VALID
 const success = async (req, res) => {
+  console.log("Success", req.body);
   try {
-    // console.log(req.body);
     const data = { val_id: req.body.val_id };
-    const sslcz = new SSLCommerzPayment(
-      process.env.STORE_ID,
-      process.env.STORE_PASSWORD,
-      is_live
-    );
-    sslcz.validate(data).then((data) => {
-      console.log("success1", data);
-      // if (data.status === "VALIID")
+    const response = await getSucess(data);
+    if(response){
       res.redirect(301, `${process.env.BASE_FRONTEND_URL}/order_done`);
-      // else res.redirect(`${process.env.BASE_FRONTEND_URL}/payment_failed`);
-    });
+    }      
   } catch (error) {
     console.log(error);
     res.status(500).send(error);
